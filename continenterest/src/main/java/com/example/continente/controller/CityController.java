fc328e976a5abf3f965d732ba21322abe7b591d7
paolo <paolo.occhionero@gmail.com>
2018-11-06 15:38:33 +0100

    bug fix citycontroller e countrycontroller

--- continenterest/src/main/java/com/example/continente/controller/CityController.java
+++ continenterest/src/main/java/com/example/continente/controller/CityController.java
@@ -1,12 +1,15 @@
 package com.example.continente.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.continente.entity.CityBean;
@@ -21,26 +24,26 @@ public class CityController {
 	@Autowired
 	private CityRepository tr;
 	
-	@GetMapping("/listacitta")
-	public List<CityBean> getCity(String nazione) {
+	@GetMapping("/listacitta/{nazione}")
+	public List<CityBean> getCity(@PathVariable("nazione") String nazione) {
 		List<CityBean> cities=tr.findByCountryCode(nazione);
 		return cities;
 	}
 	
 	@GetMapping("/cercacitta")
-	public List<CityBean> cercaCitta(String citta) {
+	public List<CityBean> cercaCitta(@RequestParam("citta") String citta) {
 		List<CityBean> cities=tr.findByNameStartingWith(citta);
 		return cities;
 	}
-	@PostMapping("/addmodify")
+	@PostMapping("/add-modify")
 	public CityBean aggiungimodifica(@RequestBody CityBean city) {
 		city=tr.save(city);
 		return city;
 	}
-	@PostMapping("/elimina")
-	public void elimina(@RequestBody CityBean city) {
+	@PostMapping("/elimina/{id}")
+	public void elimina(@PathVariable("id") Integer id) {
+		Optional<CityBean> cityOptional=tr.findById(id);
+		CityBean city=cityOptional.get();
 		tr.delete(city);
 	}
-	
-	
 }
--- continenterest/src/main/java/com/example/continente/controller/CountryController.java
+++ continenterest/src/main/java/com/example/continente/controller/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.continente.entity.CityBean;
@@ -28,7 +29,7 @@ public class CountryController {
 	}
 	
 	@GetMapping("/listanazioni")
-	public List<CountryBean> getNations(String continente) {
+	public List<CountryBean> getNations(@RequestParam("continente") String continente) {
 		List<CountryBean> nations=cr.findByContinent(continente);
 		return nations;
 	}
