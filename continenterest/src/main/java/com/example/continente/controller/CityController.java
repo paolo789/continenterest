1f9c7af14c97cc676e13e4265cf8482714bad2c4
Ivan Maltese <ivan.maltese@objectmethod.it>
2018-11-06 11:18:28 +0100

    catalogazione url

--- continenterest/src/main/java/com/example/continente/controller/CityController.java
+++ continenterest/src/main/java/com/example/continente/controller/CityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.continente.entity.CityBean;
@@ -14,6 +15,7 @@ import com.example.continente.repository.CityRepository;
 import com.example.continente.repository.CountryRepository;
 
 @RestController
+@RequestMapping("/citta")
 public class CityController {
 
 	@Autowired
